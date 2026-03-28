20abe11b2b608e8c4348adadfed5cb1268069596
Alaap Joshi <alaapjoshi@gmail.com>
2026-03-28 14:52:03 -0400

    Added arduino test servo code

--- Binsense_copy_20260328145018/Binsense_copy_20260328145018.ino
+++ Binsense_copy_20260328145018/Binsense_copy_20260328145018.ino
@@ -1,22 +1,22 @@
 
-#include <Servo.h>  // Include the Servo library
+#include <Servo.h>  
 
-Servo myservo;  // Create a servo object to control the servo motor
+Servo myservo; 
 
 void setup() {
-  myservo.attach(8);  // Attaches the servo on pin 9 to the servo object
+  myservo.attach(8);  
 }
 
 void loop() {
-  // Goes from 0 degrees to 180 degrees in steps of 1 degree
+  // Goes from 0 degrees to 180 degrees 
   for (int pos = 90; pos <= 180; pos += 1) { 
-    myservo.write(pos);  // Tell servo to go to position in variable 'pos'
-    delay(15);           // Waits for the servo to reach the position
+    myservo.write(pos);  /
+    delay(15);           
   }
 
   // Goes from 180 degrees to 0 degrees
   for (int pos = 180; pos >= 90; pos -= 1) { 
-    myservo.write(pos);  // Tell servo to go to position in variable 'pos'
-    delay(15);           // Waits for the servo to reach the position
+    myservo.write(pos);
+    delay(15);           
   }
 }
